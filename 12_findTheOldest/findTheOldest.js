c87b22d049735100e52f364ae6f36f2b936afd83
G0SU19O2 <huyquocnguyen1902@gmail.com>
2025-01-17 15:45:59 +0700

    complete Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    for (let i = 1; i < people.length; i++) {
+        let yearOfDeath = people[i].yearOfDeath || new Date().getFullYear();
+        let yearOfBirth = people[i].yearOfBirth;
+        let oldestYearOfDeath = oldest.yearOfDeath || new Date().getFullYear();
+        let oldestYearOfBirth = oldest.yearOfBirth;
+        if (yearOfDeath - yearOfBirth > oldestYearOfDeath - oldestYearOfBirth) {
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
