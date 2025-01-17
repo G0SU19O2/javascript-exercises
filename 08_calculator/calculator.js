19da045ea2f7f9b52bf549ba853e6e9406d466c8
G0SU19O2 <huyquocnguyen1902@gmail.com>
2025-01-17 15:06:47 +0700

    complete Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+  return first - second
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) {
+    return 0
+  }
+  return array.reduce((a, b) => a + b);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length === 0) {
+    return 0
+  }
+  return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	return Math.pow(num, exp)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1
+  }
+  return num * factorial(num - 1)
 };
 
 // Do not edit below this line
