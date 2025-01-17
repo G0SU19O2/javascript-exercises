56e1d7b09d0535eff32e17813e895c29a39d6070
G0SU19O2 <huyquocnguyen1902@gmail.com>
2025-01-17 15:22:36 +0700

    complete Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    const numInt = parseInt(num);
+    if (isNaN(numInt)) {
+        return "OOPS";
+    }
+    if (numInt === 0) {
+        return 0;
+    }
+    if (numInt === 1 || numInt === 2) {
+        return 1;
+    }
+    return fibonacci(numInt - 1) + fibonacci(numInt - 2);
 };
 
 // Do not edit below this line
